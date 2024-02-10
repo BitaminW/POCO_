7458253f6e11dcb36de5e15c0578c54acdd43a66
BitaminW <114838001+BitaminW@users.noreply.github.com>
2024-02-10 09:55:19 +0900

    Create ExampleStreamSocket.cpp

---
+++ chap1/ExampleStreamSocket.cpp
@@ -0,0 +1,22 @@
+#include "Poco/Net/Socket.h"
+#include "Poco/Net/StreamSocket.h"
+#include "Poco/Net/SocketStream.h"
+#include "Poco/StreamCopier.h"
+
+#include <iostream>
+
+int main() {
+
+	Poco::Net::SocketAddress sa("www.appinf.com", 80);
+	Poco::Net::StreamSocket socket(sa);
+	Poco::Net::SocketStream str(socket);
+
+	str << "GET / HTTP/1.1\r\n"
+		"Host: www.appinf.com\r\n"
+		"\r\n";
+	str.flush();
+
+	Poco::StreamCopier::copyStream(str, std::cout);
+
+	return 0;
+}
